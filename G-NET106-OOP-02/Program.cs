d9b11a600f90419c279554bfb69da7185faba582
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-16 08:56:49 +0300

    Question01

--- G-NET106-OOP-02/Program.cs
+++ G-NET106-OOP-02/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            /* A) Classes are reference types (allocated on the heap); structs are value types (allocated on the stack),
+             Classes pass references—modifications affect the shared instance. Structs pass values—modifications affect only the isolated copy,
+             We use Structs when there are lightweight data and for complex ones we use Classes.
+
+
+              B) Classes are better for large applications because they allow different components to share and edit a single 
+              piece of data in real time without making wasteful duplicate copies. They also support inheritance, 
+              which lets developers reuse and organize code cleanly as the project grows.
+             
+             */
+
+            #endregion
+
+
         }
     }
 }

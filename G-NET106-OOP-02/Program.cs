9e0b7fea7a5306c606032d5548e5c70c82249c43
Fatma_Mohamed <fm5252498@gmail.com>
2026-09-16 08:42:00 +0300

    Add project files.

---
+++ G-NET106-OOP-02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET106_OOP_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
